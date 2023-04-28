3d311f34a7046a5d78b40c9179d5ba0709c2a8ad
Alexei Samokvalov <alexei@sam701.com>
2023-04-28 10:19:59 +0200

    Make code compatible with 0.10 and 0.11

--- build.zig
+++ build.zig
@@ -1,8 +1,16 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+const zig011 = @hasDecl(std, "Build");
+const Bb = if (zig011) std.Build else std.build.Builder;
+
+pub fn build(b: *Bb) void {
+    if (zig011)
+        build_011(b)
+    else
+        build_010(b);
+}
+
+pub fn build_010(b: *Bb) void {
     const mode = b.standardReleaseOptions();
 
     const lib = b.addStaticLibrary("zig-cli", "src/main.zig");
@@ -25,6 +33,51 @@ pub fn build(b: *std.build.Builder) void {
     short.addPackagePath("zig-cli", "src/main.zig");
     short.setBuildMode(mode);
     short.install();
+
+    b.default_step.dependOn(&simple.step);
+    b.default_step.dependOn(&short.step);
+}
+
+pub fn build_011(b: *Bb) void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    const module = b.createModule(.{
+        .source_file = .{ .path = "src/main.zig" },
+    });
+
+    const lib = b.addStaticLibrary(.{
+        .name = "zig-cli",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(lib);
+
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/tests.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&main_tests.step);
+
+    const simple = b.addExecutable(.{
+        .name = "simple",
+        .root_source_file = .{ .path = "example/simple.zig" },
+        .optimize = optimize,
+    });
+    simple.addModule("zig-cli", module);
+    b.installArtifact(simple);
+
+    const short = b.addExecutable(.{
+        .name = "short",
+        .root_source_file = .{ .path = "example/short.zig" },
+        .optimize = optimize,
+    });
+    short.addModule("zig-cli", module);
+    b.installArtifact(short);
 
     b.default_step.dependOn(&simple.step);
     b.default_step.dependOn(&short.step);
