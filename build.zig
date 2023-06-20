5682d12a6e7325b7f7400b67eede5571e524b438
Alexei Samokvalov <alexei@sam701.com>
2023-06-20 06:56:38 +0200

    Build with zig-master every day

--- build.zig
+++ build.zig
@@ -42,10 +42,6 @@ pub fn build_011(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    const module = b.createModule(.{
-        .source_file = .{ .path = "src/main.zig" },
-    });
-
     const lib = b.addStaticLibrary(.{
         .name = "zig-cli",
         .root_source_file = .{ .path = "src/main.zig" },
@@ -62,6 +58,10 @@ pub fn build_011(b: *std.Build) void {
     const run_tests = b.addRunArtifact(main_tests);
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_tests.step);
+
+    const module = b.createModule(.{
+        .source_file = .{ .path = "src/main.zig" },
+    });
 
     const simple = b.addExecutable(.{
         .name = "simple",
