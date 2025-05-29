b2b52ec41a90294d0e97e3ecf05b3e99634eb70d
Alexei Samokvalov <alexei@sam701.com>
2025-05-29 22:47:26 +0200

    Add build docs target

--- build.zig
+++ build.zig
@@ -44,4 +44,16 @@ pub fn build(b: *std.Build) void {
     short.root_module.addImport("zig-cli", module);
     b.installArtifact(short);
     b.default_step.dependOn(&short.step);
+
+    // Docs
+    {
+        const install_docs = b.addInstallDirectory(.{
+            .source_dir = lib.getEmittedDocs(),
+            .install_dir = .prefix,
+            .install_subdir = "docs",
+        });
+
+        const docs_step = b.step("docs", "Install docs into zig-out/docs");
+        docs_step.dependOn(&install_docs.step);
+    }
 }
