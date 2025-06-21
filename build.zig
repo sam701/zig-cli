9d1e7e324a0e8e331078acca6d5c038a76181512
Alexei Samokvalov <alexei@sam701.com>
2025-06-21 22:30:58 +0200

    Make cli module public

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    const lib_mod = b.createModule(.{
+    const lib_mod = b.addModule("cli", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
